ae26399fff0a0e2bb1613d0ce25bd2f6ec64d5d5
Brenly <brenlyd@gmail.com>
2019-03-15 15:53:16 -0500

    .

--- mapgrep.py
+++ mapgrep.py
@@ -7,7 +7,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 import pyscreenshot as ImageGrab
-
+import pyautogui
+#import Image
 
 
 #opens csv, passes to a list data type. there were only 3-400 lines in my original data set
@@ -28,23 +29,23 @@ print(inCoordinate[i][3])
 
 #for loop starts here for now i = 3
 #url concealed for now. just test the home address!
-#searchQuery = "https://maps.google.com/@?api=1&mapaction=map&center=" + inData[i][3] + "," + inData[i][4] 
-searchQuery = "https://www.google.com/maps/search/@?api=1&mapaction=map&basemap=satellite&center=33.2076635,-97.1337569&zoom=15" ##satellite view does not work for some reason 
-#searchQuery = "https://www.google.com/maps/search/?api=1&query=33.2076635,-97.1337569&basemap=roadmap"
-#searchQuery = "https://www.google.com/maps/search/?api=1&query=33.2076635,-97.1337569&basemap=terrain"
-	
-#searchQuery = "https://www.google.com/"
+#searchQuery = "https://www.google.com/maps/search/@?api=1&mapaction=map&zoom=15&center=33.2076635,-97.1337569" ##satellite view does not work for some reason
+searchQuery = "https://www.google.com/maps/search/@?api=1&mapaction=map&zoom=15&query=" + inCoordinate[i][2] + "," + inCoordinate[i][3]
 print(searchQuery)
 driver = webdriver.Firefox()
 driver.get(searchQuery)
-sleep(2)
+sleep(1)
 driver.maximize_window()
-sleep(10)
-imageSaveLocation = inCoordinate[i][2] +  ',' + inCoordinate[i][3] + '.png'
+sleep(1)
+imageSaveLocation = inCoordinate[i][1] + "_" + inCoordinate[i][2] + ',' + inCoordinate[i][3] + '.png'
 print(imageSaveLocation)
 im = ImageGrab.grab()
 box = (0, 100, 1920, 1054)
 region = im.crop(box)
 im = region
 im.save(imageSaveLocation)
+pyautogui.moveTo(54, 999, duration=1)
+#(54, 999) #mouse move to
+pyautogui.click()
+sleep(10)
 driver.close()
